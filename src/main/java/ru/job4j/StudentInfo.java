a59c0c39517b5865046ea8ff3df7ce22c43e53bd
Антон <lopatuxin.a@gmail.com>
2024-07-19 20:40:32 +0300

    файл Multiple.java на сайт github

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Elena lopatuhina")
+        System.out.println("04.11.1987")
+    }
+}
