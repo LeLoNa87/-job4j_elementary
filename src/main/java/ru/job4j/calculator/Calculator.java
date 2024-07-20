21c9c8401aa76a82781864793e47b60467af798b
Антон <lopatuxin.a@gmail.com>
2024-07-20 21:11:29 +0300

    ru.job4j.calculate на сайт github

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,5 +7,14 @@ public class Calculator {
         int two = 2;
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+        int fourtimeTwo = four * two;
+        System.out.println(fourtimeTwo);
     }
 }
