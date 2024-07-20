65cb2c8832705d7dc52cae3dd307dbafe94f60c6
Антон <lopatuxin.a@gmail.com>
2024-07-20 23:02:56 +0300

    задача поправить код, чтобы он компилировался.
    Метод hello принимает два аргумента: первый аргумент имеет тип String, второй аргумент имеет тип int.
    
    В методе main мы вызывает 4 раза метод hello. Вам нужно написать правильные аргументы.

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,9 +2,9 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    public static void plus(int first, int second ) {
-    int result = first + second;
-    System.out.println(result);
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
