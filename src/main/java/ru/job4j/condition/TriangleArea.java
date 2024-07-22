88300fa122a1223fcf0a9c4fcccf3445e05d41f5
Антон <lopatuxin.a@gmail.com>
2024-07-22 21:43:49 +0300

    1. Реализовать метод area. Дописать туда вычисление площади по формуле Герона.

---
+++ src/main/java/ru/job4j/condition/TriangleArea.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class TriangleArea {
+    public  static double area(double a, double b, double c) {
+
+        double p = (a + b + c) / 2;
+        double s = Math.sqrt(16);
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double result = TriangleArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + result);
+
+    }
+}
