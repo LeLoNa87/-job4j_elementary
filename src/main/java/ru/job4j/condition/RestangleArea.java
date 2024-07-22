aa648098a2b0605e9b3108cced4ad311102b2b21
Антон <lopatuxin.a@gmail.com>
2024-07-22 21:29:59 +0300

    1. Создайте класс ru.job4j.condition.RectangleArea. Допишите метод square, используя три формулы для h, l и s. 2. Доработайте метод main с демонстрацией работы программы с параметрами p = 6, k = 2, площадь должна быть 2.
    Вам нужно заменить -1 на формулу для вычисления валюты.
    
    1 доллар = 60 рублей.
    1 евро = 70 рублей.
    
    1. Доработайте методы manWeight и womanWeight.
    
    Нужно дописать код, чтобы методы вычисляли идеальный вес для мужчины и женщины.
    Вместо -1 вам нужно написать формулу приведенную выше.

---
+++ src/main/java/ru/job4j/condition/RestangleArea.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class RestangleArea {
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l =  h * k;
+        double s = l * h;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double result = RestangleArea.square(4, 1);
+        double result1 = RestangleArea.square(6, 2);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
