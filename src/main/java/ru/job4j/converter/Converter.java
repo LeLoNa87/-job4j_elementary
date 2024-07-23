2d24a0b0a8b251f0cee309ffa36a6d0448d59705
Антон <lopatuxin.a@gmail.com>
2024-07-23 22:12:34 +0300

    1. Доработайте код в методе main класса ru.job4j.converter.Converter. Нужно добавить проверку остальных методов. При этом:
    - код из задания по метода конвертации евро в окончательном варианте также должен быть;
    
    - для проверки метода конвертации подберите такое исходное значение, которое при делении даст в итоге целое число.

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,27 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEvro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = value / 60;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float input = 140;
+        float input1 = 120;
+        float expected = 2;
+        float expected1 = 2;
+        float output = Converter.rubleToEvro(input);
+        float output1 = Converter.rubleToDollar(input1);
+        boolean passed = expected == output;
+        boolean passed1 = expected1 == output1;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("120 rubles are 2. test result1 : " + passed1);
+
+    }
+}
