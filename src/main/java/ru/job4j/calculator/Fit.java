39bcbd24e5d8c8d4921efe7879e2cb21f7f5916b
Антон <lopatuxin.a@gmail.com>
2024-07-25 22:05:04 +0300

    1. Создайте класс ru.job4j.calculator.FitTest.
    2. Создайте в нем тест-методы для проверки идеального веса для мужчины и женщины. В качестве исходных данных для расчет используйте значения среднего роста мужчины и женщины, или те значения которые предложены в каркасе в названии тестов.

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -4,7 +4,6 @@ public class Fit {
     public static double manWeight(short height) {
         double result = (height - 100) * 1.15;
         return result;
-
     }
 
     public static double womanWeight(short height) {
@@ -13,8 +12,11 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        double man = Fit.manWeight(height);
+        short heightMan = 187;
+        double man = Fit.manWeight(heightMan);
         System.out.println("Man 187 is " + man);
+        short heightWoman = 170;
+        double woman = Fit.womanWeight(heightWoman);
+        System.out.println("Woman 170 is " + woman);
     }
 }
